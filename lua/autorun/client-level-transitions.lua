1697acf67688372b214095ad4e8c7034308367f1
ICEMAN <iceman.twitch.contact@gmail.com>
2025-02-12 18:46:49 +0100

    Update client-level-transitions.lua

--- lua/autorun/client-level-transitions.lua
+++ lua/autorun/client-level-transitions.lua
@@ -30,63 +30,103 @@
     -- Improve map detection and screenshot file naming for better organization.
 
 if CLIENT then
-local tag = "[HL2-LoadingScreen]: "
-local author = "iceman_twitch"
-local email = "iceman.twitch.contact@gmail.com"
-local website = "linktr.ee/iceman_twitch"
-local version = "0.13v 2025/02/11-13:28"
-local aaaaaaaaaa = print
-local function print(...)
-    local a = ...
-    local b = tag .. a
-    aaaaaaaaaa(b)
-end
-local lEP, lEA
-hook.Add("PostDrawTranslucentRenderables", "LevelTransitions_EyePos", function()
-    lEP, lEA = EyePos(), EyeAngles() 
-end)
-hook.Add("InitPostEntity", "LevelTransition_ImageDelete", function()
-    file.Delete("map_transition.png")
-end)
-print("author: "..author)
-print("email: "..email)
-print("website: "..website)
-print("version: "..version)
-print"Init"
-function LevelTransitions_Capture()
-    print"Capture Called"
-    gui.HideGameUI()
-    file.Delete("hl2-campaign-loading/map_transition.png")
-    print"Deleted Previous Image"
-    timer.Simple( 0, function()
-        
-        if not file.Exists("hl2-campaign-loading", "DATA") then
-            file.Write"hl2-campaign-loading"
-        end
-        render.RenderView({
-              origin = lEP, angles = lEA,
-              x = 0, y = 0,
-              w = ScrW(), h = ScrH(),
-              zfar = 16000;
-              -- do you need FOV? nah lol
-            })  
-        local data = render.Capture( {
-            format = "png",
-            x = 0,
-            y = 0,
-            w = ScrW(),
-            h = ScrH(),
-            quality = 70, -- 
-            alpha = false, -- only needed for the png format to prevent the depth buffer leaking in, see BUG
-        } )
-        print"Captured Screenshot"
-        file.Write("hl2-campaign-loading/map_transition.png", data )
-        print"Saved Screenshot"
+    local tag = "[HL2-LoadingScreen]: "
+    local author = "iceman_twitch"
+    local email = "iceman.twitch.contact@gmail.com"
+    local website = "linktr.ee/iceman_twitch"
+    local version = "0.13v 2025/02/11-13:28"
+    local aaaaaaaaaa = print
+    local shutdown = false
+    local function print(...)
+        local a = ...
+        local b = tag .. a
+        aaaaaaaaaa(b)
+    end
+    local lEP, lEA
+    hook.Add("PostDrawTranslucentRenderables", "LevelTransitions_EyePos", function()
+        lEP, lEA = EyePos(), EyeAngles() 
     end)
-end
--- hook.Add("ShutDown", "LevelTransitions_DoCapture", LevelTransitions_Capture)
-net.Receive( "trigger_changelevel_touch", function( len, ply )
-    LevelTransitions_Capture()
-end)
+    hook.Add("InitPostEntity", "LevelTransition_ImageDelete", function()
+        file.Delete("hl2-campaign-loadingscreen/map_transition.png") 
+    end)
+    print("author: "..author)
+    print("email: "..email)
+    print("website: "..website)
+    print("version: "..version)
+    print"Init"
 
+
+    function LevelTransitions_Capture()
+        if shutdown then return end
+        print"Capture Called"
+        if file.Exists("hl2-campaign-loadingscreen/map_transition.png","DATA") then 
+            file.Delete("hl2-campaign-loadingscreen/map_transition.png") 
+            print"Deleted Previous Image"
+        end
+        if not file.Exists("hl2-campaign-loadingscreen", "DATA") then
+            file.CreateDir"hl2-campaign-loadingscreen"
+        end
+        gui.HideGameUI()
+        timer.Simple( 0, function()
+            
+            
+            render.RenderView({
+                  origin = lEP, angles = lEA,
+                  x = 0, y = 0,
+                  w = ScrW(), h = ScrH(),
+                  zfar = 16000;
+                  -- do you need FOV? nah lol
+                })  
+            local data = render.Capture( {
+                format = "png",
+                x = 0,
+                y = 0,
+                w = ScrW(),
+                h = ScrH(),
+                quality = 70, -- 
+                alpha = false, -- only needed for the png format to prevent the depth buffer leaking in, see BUG
+            } )
+            print"Captured Screenshot"
+            file.Write("hl2-campaign-loadingscreen/map_transition.png", data )
+            print"Saved Screenshot"
+        end)
+    end
+    net.Receive( "trigger_changelevel_touch", function( len, ply )
+        LevelTransitions_Capture()
+    end)
+    -- Trying to fix dis but not gonna work at all.
+    if game.SinglePlayer() then
+        hook.Add("ShutDown", "LevelTransition_ShutDown", function()
+            shutdown = true
+            print"Shutdown"
+            print"Capture Called"
+            if file.Exists("hl2-campaign-loadingscreen/map_transition.png","DATA") then 
+                file.Delete("hl2-campaign-loadingscreen/map_transition.png") 
+                print"Deleted Previous Image"
+            end
+            if not file.Exists("hl2-campaign-loadingscreen", "DATA") then
+                file.CreateDir"hl2-campaign-loadingscreen"
+            end
+            gui.HideGameUI()
+            render.RenderView({
+                  origin = lEP, angles = lEA,
+                  x = 0, y = 0,
+                  w = ScrW(), h = ScrH(),
+                  zfar = 16000;
+                  -- do you need FOV? nah lol
+                })  
+            local data = render.Capture( {
+                format = "png",
+                x = 0,
+                y = 0,
+                w = ScrW(),
+                h = ScrH(),
+                quality = 70, -- 
+                alpha = false, -- only needed for the png format to prevent the depth buffer leaking in, see BUG
+            } )
+            print"Captured Screenshot"
+            file.Write("hl2-campaign-loadingscreen/map_transition.png", data )
+            print"Saved Screenshot"
+        end)
+    end
 end
